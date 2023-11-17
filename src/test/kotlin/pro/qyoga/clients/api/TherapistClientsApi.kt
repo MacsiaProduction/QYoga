804516789b806f96f056ecd750fc856685b39364
MacsiaProduction <Macsia.Production@gmail.com>
2023-11-17 21:11:36 +0700

    feat Добавлен метод TherapistClientsApi.getCreateClientPage() Добавлен объект CreateClientPage

--- src/test/kotlin/pro/qyoga/clients/api/TherapistClientsApi.kt
+++ src/test/kotlin/pro/qyoga/clients/api/TherapistClientsApi.kt
@@ -9,8 +9,9 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.springframework.http.HttpStatus
 import pro.qyoga.clients.pages.therapist.clients.ClientsListPage
+import pro.qyoga.clients.pages.therapist.clients.CreateClientPage
 import pro.qyoga.core.clients.api.ClientSearchDto
-
+import pro.qyoga.core.clients.api.CreateClientRequest
 
 class TherapistClientsApi(override val authCookie: Cookie) : AuthorizedApi {
 
@@ -18,7 +19,19 @@ class TherapistClientsApi(override val authCookie: Cookie) : AuthorizedApi {
         return Given {
             authorized()
         } When {
-            get(ClientsListPage.path)
+            get(CreateClientPage.path)
+        } Then {
+            statusCode(HttpStatus.OK.value())
+        } Extract {
+            Jsoup.parse(body().asString())
+        }
+    }
+
+    fun getCreateClientPage() : Document {
+        return Given {
+            authorized()
+        } When {
+            get(CreateClientPage.path)
         } Then {
             statusCode(HttpStatus.OK.value())
         } Extract {
@@ -31,7 +44,7 @@ class TherapistClientsApi(override val authCookie: Cookie) : AuthorizedApi {
             authorized()
             formParam(ClientsListPage.ClientSearchForm.firstName.name, searchForm.firstName)
             formParam(ClientsListPage.ClientSearchForm.lastName.name, searchForm.lastName)
-            formParam(ClientsListPage.ClientSearchForm.patronymic.name, searchForm.patronymic)
+            formParam(ClientsListPage.ClientSearchForm.patronymic.name, searchForm.middleName)
             formParam(ClientsListPage.ClientSearchForm.phoneNumber.name, searchForm.phoneNumber)
         } When {
             get(ClientsListPage.ClientSearchForm.action.url)
